2cab1690615fc28412273f231ff16c840d35ddf0
hamster1987 <january16@tut.by>
2014-10-05 15:14:50 +0300

    minor changes affecting formatting

--- epam_entry_task3.php
+++ epam_entry_task3.php
@@ -21,17 +21,15 @@ echo checkInput('wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww50symbolswwwwwwwwwwwww
 	wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww200symbolsffffffffffffffffffffffff') . '<br>';//... 150symbol.www....
 
 //the function itself follows below: returns FALSE if the filename or extension contain no permitted characters or no dots(.), i.e. cannot be rendered as a legal file name without adding something to it 
-function checkInput($str)
-{
-	$str = preg_replace('/[^a-z_0-9.]/', "", $str);// remove all but permitted entities
-	$str = preg_replace('/\.(?=.*\.)/', "", $str);// if there is a dot preceding any numner of any entities and a dot, remove the first of the two dots (removes all but the last of the dots) 
-	$str = preg_replace('/((?<=\.).{50}).*/', "$1", $str);// leave only 50 characters after the dot, remove the rest of the characters following after the dot
+function checkInput($str) {
+	$str = preg_replace('/[^a-z_0-9.]/', '', $str);// remove all but permitted entities
+	$str = preg_replace('/\.(?=.*\.)/', '', $str);// if there is a dot preceding any numner of any entities and a dot, remove the first of the two dots (removes all but the last of the dots) 
+	$str = preg_replace('/((?<=\.).{50}).*/', '$1', $str);// leave only 50 characters after the dot, remove the rest of the characters following after the dot
 	$nameExt=explode('.', $str);// divide the string into two parts: file name and file extension
 	$name = $nameExt[0];
 	$extension = $nameExt[1];
 	if ($name == '' || $extension == '' || strpos($str, '.') == NULL) return FALSE;// return FALSE in eithe case: file name is empty, file extension is empty, no dots contained in the string (cannot separate file name from its extension) 
-	if (strlen($str) > 200)//if the string is too long, remove as little as possible from its name (the extension is already no longer than 50 characters) 
-	{
+	if (strlen($str) > 200) {//if the string is too long, remove as little as possible from its name (the extension is already no longer than 50 characters) 
 		$name = substr($name, 0, 200-strlen($extension)-1);
 	}
 	$str = $name.".".$extension;
